ca3a2d3f6a31fac2ea0265676098f6b52b0dfb4b
v01d <Kevin_Vearez@mail.ru>
2026-02-08 20:48:47 +0700

    Add structure for power calculation task

--- lab_01/main.cs
+++ lab_01/main.cs
@@ -6,6 +6,17 @@ namespace BaseCSharpTasks
   {
     static void Main(string[] args)
     {
+    // Task 1: Power calculation using only multiplication
+
+    int baseNumber;
+    int powerValue;
+    long powerResult;
+
+    Console.Write("Enter base number (a): ");
+    baseNumber = int.Parse(Console.ReadLine());
+
+    Console.Write("Enter power value (n): ");
+    powerValue = int.Parse(Console.ReadLine());
     }
   }
 }
