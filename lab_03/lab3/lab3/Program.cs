48437c869ab3fc238e922aa3821051e58cf89d63
v01d <Kevin_Vearez@mail.ru>
2026-03-02 15:50:29 +0700

    Init project: define custom exceptions and IMyCloneable interface

---
+++ lab_03/lab3/lab3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace MatrixCalculator {
+  public class MatrixException : Exception {
+    public MatrixException(string message) : base(message) { }
+  }
+
+  public class SizeMismatchException : MatrixException {
+    public SizeMismatchException(string message) : base(message) { }
+  }
+
+  public class SingularMatrixException : MatrixException {
+    public SingularMatrixException(string message) : base(message) { }
+  }
+
+  public interface IMyCloneable {
+    object Clone();
+  }
+}
