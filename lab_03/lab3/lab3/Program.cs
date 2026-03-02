dfcfe232ab74e8a3821f73ea35edef334315a4b0
v01d <Kevin_Vearez@mail.ru>
2026-03-02 16:26:32 +0700

    Implement SquareMatrix core and Prototype pattern

--- lab_03/lab3/lab3/Program.cs
+++ lab_03/lab3/lab3/Program.cs
@@ -2,7 +2,6 @@
 using System.Text;
 
 namespace MatrixCalculator {
-  // CUSTOM EXCEPTIONS BLOCK
   public class MatrixException : Exception {
     public MatrixException(string message) : base(message) { }
   }
@@ -15,15 +14,65 @@ namespace MatrixCalculator {
     public SingularMatrixException(string message) : base(message) { }
   }
 
-  // PROTOTYPE PATTERN INTERFACE
   public interface IMyCloneable {
     object Clone();
   }
 
+  public class SquareMatrix : IMyCloneable {
+    // DECLARATION BLOCK
+    private double[,] matrixData;
+    private int matrixSize;
+
+    // CONSTRUCTORS
+    public SquareMatrix(int size) {
+      if (size <= 0) {
+        throw new MatrixException("Size must be greater than zero.");
+      }
+
+      matrixSize = size;
+      matrixData = new double[size, size];
+    }
+
+    public SquareMatrix(int size, int minValue, int maxValue) : this(size) {
+      // DECLARATION BLOCK
+      Random randomGenerator;
+      randomGenerator = new Random();
+
+      // CALCULATION BLOCK
+      for (int rowIndex = 0; rowIndex < size; rowIndex++) {
+        for (int columnIndex = 0; columnIndex < size; columnIndex++) {
+          matrixData[rowIndex, columnIndex] = (double)randomGenerator.Next(minValue, maxValue);
+        }
+      }
+    }
+
+    public int Size {
+      get { return matrixSize; }
+    }
+
+    public double this[int rowIndex, int columnIndex] {
+      get { return matrixData[rowIndex, columnIndex]; }
+      set { matrixData[rowIndex, columnIndex] = value; }
+    }
+
+    public object Clone() {
+      // DECLARATION BLOCK
+      SquareMatrix clonedMatrix;
+      clonedMatrix = new SquareMatrix(this.matrixSize);
+
+      // CALCULATION BLOCK
+      for (int rowIndex = 0; rowIndex < matrixSize; rowIndex++) {
+        for (int columnIndex = 0; columnIndex < matrixSize; columnIndex++) {
+          clonedMatrix[rowIndex, columnIndex] = this.matrixData[rowIndex, columnIndex];
+        }
+      }
+      return clonedMatrix;
+    }
+  }
+
   class Program {
     static void Main(string[] args) {
-      // INITIALIZATION BLOCK
-      Console.WriteLine("System: Infrastructure initialized.");
+      Console.WriteLine("System: SquareMatrix class added.");
     }
   }
 }
