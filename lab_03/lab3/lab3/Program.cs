2bc2fb571af7a146669d7da027700f8b7bb403e0
v01d <Kevin_Vearez@mail.ru>
2026-03-02 15:51:16 +0700

    Add SquareMatrix core: constructors, indexer and random generation

--- lab_03/lab3/lab3/Program.cs
+++ lab_03/lab3/lab3/Program.cs
@@ -1,20 +1,34 @@
 ﻿using System;
-using System.Text;
 
-namespace MatrixCalculator {
-  public class MatrixException : Exception {
-    public MatrixException(string message) : base(message) { }
+public class SquareMatrix : IMyCloneable, IComparable<SquareMatrix> {
+  private double[,] matrixData;
+  private int matrixSize;
+
+  public SquareMatrix(int size) {
+    if (size <= 0) {
+      throw new MatrixException("Size must be greater than zero.");
+    }
+    matrixSize = size;
+    matrixData = new double[size, size];
   }
 
-  public class SizeMismatchException : MatrixException {
-    public SizeMismatchException(string message) : base(message) { }
+  public SquareMatrix(int size, int minValue, int maxValue) : this(size) {
+    // DECLARATION BLOCK
+    Random randomGenerator;
+    randomGenerator = new Random();
+
+    // CALCULATION BLOCK
+    for (int rowIndex = 0; rowIndex < size; rowIndex++) {
+      for (int columnIndex = 0; columnIndex < size; columnIndex++) {
+        matrixData[rowIndex, columnIndex] = (double)randomGenerator.Next(minValue, maxValue);
+      }
+    }
   }
 
-  public class SingularMatrixException : MatrixException {
-    public SingularMatrixException(string message) : base(message) { }
-  }
+  public int Size { get { return matrixSize; } }
 
-  public interface IMyCloneable {
-    object Clone();
+  public double this[int rowIndex, int columnIndex] {
+    get { return matrixData[rowIndex, columnIndex]; }
+    set { matrixData[rowIndex, columnIndex] = value; }
   }
 }
