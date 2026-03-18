929563d67b0bc94b51388e73bfec25fa5e07220d
v01d <Kevin_Vearez@mail.ru>
2026-03-18 14:02:34 +0700

    init
    Created the basic TextDocument class to hold file data and implemented a simple console menu stub.

--- lab_04/lab4/lab4/Program.cs
+++ lab_04/lab4/lab4/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace lab4 {
-  internal class Program {
+namespace TextEditorLab {
+  // Base class to represent a text file
+  public class TextDocument {
+    public string fileName { get; set; }
+    public string content { get; set; }
+
+    public TextDocument() {
+      fileName = "Untitled.txt";
+      content = string.Empty;
+    }
+  }
+
+  class Program {
     static void Main(string[] args) {
+      bool isRunning;
+      string userChoice;
+
+      isRunning = true;
+
+      while (isRunning) {
+        Console.Clear();
+        Console.WriteLine("=== TEXT EDITOR v1.0 ===");
+        Console.WriteLine("1. Open Editor");
+        Console.WriteLine("0. Exit");
+        Console.Write("Choice: ");
+
+        userChoice = Console.ReadLine();
+
+        if (userChoice == "1") {
+          Console.WriteLine("Editor logic will be here...");
+          Console.ReadLine();
+        } else if (userChoice == "0") {
+          isRunning = false;
+        }
+      }
     }
   }
 }
