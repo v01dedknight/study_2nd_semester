f5dbacc0046de4371f2525af20d2ac2f45bd71a3
v01d <Kevin_Vearez@mail.ru>
2026-03-18 14:03:08 +0700

    Added XML and Binary serialization
    Implemented Save/Load methods for XML and Binary formats. Added FileStream handling.

--- lab_04/lab4/lab4/Program.cs
+++ lab_04/lab4/lab4/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace TextEditorLab {
-  // Base class to represent a text file
   public class TextDocument {
     public string fileName { get; set; }
     public string content { get; set; }
@@ -11,30 +12,59 @@ namespace TextEditorLab {
       fileName = "Untitled.txt";
       content = string.Empty;
     }
+
+    public TextDocument(string name, string text) {
+      fileName = name;
+      content = text;
+    }
+
+    // Standard XML serialization
+    public void SaveToXml(string path) {
+      XmlSerializer serializer;
+      serializer = new XmlSerializer(typeof(TextDocument));
+
+      using (FileStream stream = new FileStream(path, FileMode.Create)) {
+        serializer.Serialize(stream, this);
+      }
+    }
+
+    public static TextDocument LoadFromXml(string path) {
+      XmlSerializer serializer;
+      serializer = new XmlSerializer(typeof(TextDocument));
+
+      using (FileStream stream = new FileStream(path, FileMode.Open)) {
+        return (TextDocument)serializer.Deserialize(stream);
+      }
+    }
+
+    // Manual Binary serialization for better control
+    public void SaveToBinary(string path) {
+      using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create))) {
+        writer.Write(fileName);
+        writer.Write(content);
+      }
+    }
+
+    public static TextDocument LoadFromBinary(string path) {
+      string name;
+      string text;
+
+      using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open))) {
+        name = reader.ReadString();
+        text = reader.ReadString();
+        return new TextDocument(name, text);
+      }
+    }
   }
 
   class Program {
     static void Main(string[] args) {
       bool isRunning;
-      string userChoice;
-
       isRunning = true;
 
       while (isRunning) {
-        Console.Clear();
-        Console.WriteLine("=== TEXT EDITOR v1.0 ===");
-        Console.WriteLine("1. Open Editor");
-        Console.WriteLine("0. Exit");
-        Console.Write("Choice: ");
-
-        userChoice = Console.ReadLine();
-
-        if (userChoice == "1") {
-          Console.WriteLine("Editor logic will be here...");
-          Console.ReadLine();
-        } else if (userChoice == "0") {
-          isRunning = false;
-        }
+        Console.WriteLine("Menu stub...");
+        isRunning = false;
       }
     }
   }
